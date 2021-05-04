748383291f0125c9f89dfc58305e83e115739bf4
Rizqi Pratama <rev4rmy@gmail.com>
2021-05-04 16:11:49 +0700

    evoucher sql server update

--- routes/evoucherperincian.js
+++ routes/evoucherperincian.js
@@ -15,9 +15,11 @@ var router = express.Router();
 
 router.get('/', async function(req, res, next) {
     try {
-        const lastsync = await axios.get('http://localhost:3003/users');
-        const querydata = await conn.query('SELECT TOP 10  *FROM tEVoucherPerincian');
-        res.send(querydata[0].time);
+        const lastsync = await axios.get('http://localhost:3003/evoucherperincian');
+        hari = lastsync.data.data.date 
+        waktu =  lastsync.data.data.time
+        const querydata = await conn.query(`SELECT TOP 10 CONVERT(date, Tanggal) as dt, CONVERT(time, Tanggal) as tm , dt + tm as Tanggal FROM tEVoucherPerincian WHERE date='${hari}'`);
+        res.send(querydata);
       } catch (error) {
         console.error(error);
       }
