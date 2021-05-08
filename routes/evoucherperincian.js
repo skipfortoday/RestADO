424c04cb54096d336edc52a8b81eefe57dd4fd4c
Rizqi Pratama <rev4rmy@gmail.com>
2021-05-08 10:14:12 +0700

    Fix untuk kondisi Query SQL Server
    Menambahkan table CreateAt pada sql server dan fix kondisi query untuk data yang diambil

--- routes/evoucherperincian.js
+++ routes/evoucherperincian.js
@@ -4,6 +4,14 @@ const moment = require("moment");
 const conn = require("../app");
 const router = express.Router();
 
+setInterval(function(){ axios.get('http://localhost:3000/evoucherperincian')
+.then(function (response) {
+  console.log(response.data);
+})
+.catch(function (error) {
+  console.log('err');
+}); }, 3000);
+
 router.get("/", async function (req, res, next) {
   try {
     const lastsync = await axios.get("http://localhost:3003/evoucherperincian");
@@ -11,19 +19,18 @@ router.get("/", async function (req, res, next) {
     WaktuTerakhirSync = lastsync.data.data.time;
     let TglSekarang = moment.parseZone(moment()).format("YYYY-MM-DD");
     let WaktuSekarang = moment.parseZone(moment()).format("HH:mm:ss");
-    const querydata = await conn.query(`SELECT RecordNum,
+    const querydata = await conn.query  (`SELECT TOP 300 RecordNum,
                                         CONVERT(date, Tanggal) as Tgldate,
                                         CONVERT(time, Tanggal) as Tgltime,
                                         NoBukti,Keterangan,AmountD,AmountK,
                                         SaldoAwal,SaldoAkhir,IndexNum,UserID,
                                         CONVERT(date, TglInput) as TglInputdate,
                                         CONVERT(time, TglInput) as TglInputtime,
-                                        Ubah,Hapus,Pelanggan,Lokasi,EVoucher,
-                                        Koreksi,time,date
+                                        Ubah,Hapus,Pelanggan,Lokasi,EVoucher,Koreksi
                                         FROM tEVoucherPerincian
-                                        WHERE date BETWEEN '${TglTerakhirSync}' AND '${TglSekarang}' 
-                                        AND time BETWEEN '${WaktuTerakhirSync}' AND '${WaktuSekarang}'
-                                      `);
+                                        WHERE CreateAt BETWEEN '${TglTerakhirSync}${' '}${WaktuTerakhirSync}' 
+                                        AND '${TglSekarang}${' '}${WaktuSekarang}'
+                                      `);                                  
   let dataTcard = '';
   querydata.forEach(items => {
     dataTcard += 
