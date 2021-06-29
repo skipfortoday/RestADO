9dab7d3b812cc03c8732ed10b9d93ab71d676ff5
Rizqi Pratama <rev4rmy@gmail.com>
2021-06-29 16:08:44 +0700

    menambahkan api dokter

--- routes/kartu-pasien/dokter/index.js
+++ routes/kartu-pasien/dokter/index.js
@@ -4,40 +4,34 @@ const moment = require("moment");
 const sqlkp = require("../../../sqlkartupasien");
 const router = express.Router();
 
-// setInterval(function () {
-//   axios
-//     .get("http://localhost:4000/kartu-pasien/dokter")
-//     .then(function (response) {
-//       console.log(response.data);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// }, 3000);
+setInterval(function () {
+  axios
+    .get("http://localhost:4000/kartu-pasien/dokter")
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}, 3000);
 
 router.get("/", async function (req, res, next) {
   try {
-    const lastsync = await axios.get(
-      "http://localhost:3000/api/kartu-pasien/dokter/waktu"
-    );
-    WaktuTerakhirSync = lastsync.data.data;
-    console.log(WaktuTerakhirSync);
     let q = `
     SELECT TOP 100 
     IDDOkter,NamaDokter,Status,Exported,
     CONVERT(date, TglAuto) as dateTglAuto,
     CONVERT(time, TglAuto) as timeTglAuto
     FROM tblDokter
-    WHERE TglAuto > '${WaktuTerakhirSync}.59' 
+    WHERE TglAuto > (SELECT CONVERT(varchar, "time", 120)+'.999' 
+    FROM timeAnchor Where tablekey='tblDokter');
   `;
     const querydata = await sqlkp.query(q);
     if (querydata[0]) {
       let dataTcard = "";
       querydata.forEach((items) => {
         dataTcard += `(
-          '${items.IDDOkter}',
-          '${items.NamaDokter}',
-          '${items.Status}',
+          '${items.IDDOkter}','${items.NamaDokter}','${items.Status}',
           '${items.Exported}',
           '${items.dateTglAuto} ${items.timeTglAuto.substring(
           0,
@@ -50,8 +44,24 @@ router.get("/", async function (req, res, next) {
         .post("http://localhost:3000/api/kartu-pasien/dokter/data/", {
           data: dataTcard,
         })
-        .then(function (response) {
-          res.send(response.data);
+        .then(async function (response) {
+          try {
+            const lastsync = await axios.get(
+              "http://localhost:3000/api/kartu-pasien/dokter/waktu"
+            );
+            WaktuTerakhirSync = lastsync.data.data;
+            console.log(`UPDATE "timeAnchor" set
+            "time" = '${WaktuTerakhirSync}' 
+            WHERE tablekey='tblDokter'`);
+            let querydata = await sqlkp.execute(
+              `UPDATE "timeAnchor" set
+              "time" = '${WaktuTerakhirSync}' 
+              WHERE tablekey='tblDokter';`
+            );
+            res.send(querydata);
+          } catch (error) {
+            res.send(error);
+          }
         })
         .catch(function (error) {
           res.send(error);
